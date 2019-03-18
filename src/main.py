a101fcfbe63515ef810e8e4205d894f742c0ec5f
Lomaev <danil.lomaev.2003@yandex.ru>
2019-03-18 13:00:12 +0400

    1.0. Release! Chat without registration and any other stuff. But it works!

--- src/main.py
+++ src/main.py
@@ -1,27 +1,29 @@
 import sys
 sys.path.insert(0, sys.path[0] + '\\DB')
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from DB_code_core import DB, MessagesModel
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 messages_DB = DB('messages')
 messages_model = MessagesModel(messages_DB.get_connection())
 messages_model.init_table()
-messages_model.insert()
-messages_model.insert(user_name='Masha', content='NO')
-messages_model.insert(user_name='Admin', content='Yes')
-print(messages_model.get_all())
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('chat_window.html')
+    session['username'] = 'Anon'
+    if request.method == 'POST':
+        session['username'] = request.form.get('username')
+        messages_model.insert(user_name=request.form.get('username'), content=request.form.get('content'))
+    return render_template('chat_window.html', username=session['username'] )
 
 
-@app.route('/API/chat_content')
+@app.route('/API/chat_content', methods=['GET', 'POST'])
 def content():
-    return render_template('chat_content.html', messages=messages_model.get_all())
+    return render_template('chat_content.html', messages=messages_model.get_all()[-10:])
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=80, host='127.0.0.1')
